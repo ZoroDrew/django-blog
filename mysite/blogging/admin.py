19297d6028eac56d9f279855f5b6bc7592bfbcb3
Drew <ZoroDrew@users.noreply.github.com>
2024-03-17 17:19:45 -0700

    Lesson 7 assignment

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,8 +1,25 @@
-from django.contrib import admin
+from django.db import models
+from django.contrib.auth.models import User
 
-from blogging.models import Post, Category
+class Post(models.Model):
 
-# Register your models here.
+    title = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-admin.site.register(Post)
-admin.site.register(Category)
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories' 
